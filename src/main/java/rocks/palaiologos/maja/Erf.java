52a41375cf5bcd8d871bf9650894b360a902d6f7
Kamila Szewczyk <kspalaiologos@gmail.com>
2023-03-25 18:38:51 +0100

    fix erfc

--- src/main/java/rocks/palaiologos/maja/Erf.java
+++ src/main/java/rocks/palaiologos/maja/Erf.java
@@ -429,25 +429,7 @@ class Erf {
     }
 
     public static double erfc(double x) {
-        if(x == 0) return 1.0;
-        else if(x == Double.NEGATIVE_INFINITY) return 2.0;
-
-        final double t = 1.0 / (1.0 + 0.5 * Math.abs(x));
-        final double ans = t
-                * Math.exp(-x
-                * x
-                - 1.26551223
-                + t
-                * (1.00002368 + t
-                * (0.37409196 + t
-                * (0.09678418 + t
-                * (-0.18628806 + t
-                * (0.27886807 + t
-                * (-1.13520398 + t
-                * (1.48851587 + t
-                * (-0.82215223 + t * 0.17087277)))))))));
-
-        return x >= 0.0 ? ans : -ans;
+        return 1.0 - erf(x);
     }
 
     public static double inverfc(double p) {
--- src/test/java/TestErf.java
+++ src/test/java/TestErf.java
@@ -25,11 +25,43 @@ public class TestErf {
         assertThat(Maja.erfc(Double.NaN)).isNaN();
         assertThat(Maja.erfc(Double.POSITIVE_INFINITY)).isEqualTo(0);
         assertThat(Maja.erfc(Double.NEGATIVE_INFINITY)).isEqualTo(2);
-        assertThat(Maja.erfc(0.5)).isEqualTo(0.47950009227675744);
-        assertThat(Maja.erfc(-0.5)).isEqualTo(-0.47950009227675744);
-        assertThat(Maja.erfc(1.5)).isEqualTo(0.033894850349524795);
-        assertThat(Maja.erfc(12.34)).isEqualTo(3.359252262812403E-68);
-        assertThat(Maja.erfc(-2.0)).isEqualTo(-0.004677734989334044);
-        assertThat(Maja.erfc(0.111)).isEqualTo(0.8752624198776775);
+        assertThat(Maja.erfc(0.5)).isEqualTo(0.4795001221869536);
+        assertThat(Maja.erfc(-0.5)).isEqualTo(1.5204998778130463);
+        assertThat(Maja.erfc(1.5)).isEqualTo(0.03389485352468924);
+        assertThat(Maja.erfc(12.34)).isEqualTo(0.0);
+        assertThat(Maja.erfc(-2.0)).isEqualTo(1.9953222650189528);
+        assertThat(Maja.erfc(0.111)).isEqualTo(0.8752624187419497);
+    }
+
+    @Test
+    public void testErfi() {
+        assertThat(Maja.erfi(0)).isEqualTo(0);
+        assertThat(Maja.erfi(Double.NaN)).isNaN();
+        assertThat(Maja.erfi(Double.POSITIVE_INFINITY)).isEqualTo(Double.POSITIVE_INFINITY);
+        assertThat(Maja.erfi(Double.NEGATIVE_INFINITY)).isEqualTo(Double.NEGATIVE_INFINITY);
+        assertThat(Maja.erfi(0.5)).isEqualTo(0.6149520946965109);
+        assertThat(Maja.erfi(-0.5)).isEqualTo(-0.6149520946965109);
+        assertThat(Maja.erfi(1.5)).isEqualTo(4.5847332572844275);
+        assertThat(Maja.erfi(12.34)).isEqualTo(6.223011839236671E64);
+        assertThat(Maja.erfi(-2.0)).isEqualTo(-18.564802414575553);
+        assertThat(Maja.erfi(0.111)).isEqualTo(0.1257663966329148);
+    }
+
+    @Test
+    public void testErfInv() {
+        assertThat(Maja.abs(Maja.erfInv(Maja.erf(0)) - 0)).isLessThan(3e-8);
+        assertThat(Maja.abs(Maja.erfInv(Maja.erf(1)) - 1)).isLessThan(3e-8);
+        assertThat(Maja.abs(Maja.erfInv(Maja.erf(0.5)) - 0.5)).isLessThan(4e-8);
+        assertThat(Maja.abs(Maja.erfInv(Maja.erf(-0.5)) + 0.5)).isLessThan(4e-8);
+        assertThat(Maja.abs(Maja.erfInv(Maja.erf(0.111)) - 0.111)).isLessThan(4e-8);
+    }
+
+    @Test
+    public void testErfcInv() {
+        assertThat(Maja.abs(Maja.erfcInv(Maja.erfc(0)) - 0)).isLessThan(3e-8);
+        assertThat(Maja.abs(Maja.erfcInv(Maja.erfc(1)) - 1)).isLessThan(3e-8);
+        assertThat(Maja.abs(Maja.erfcInv(Maja.erfc(0.5)) - 0.5)).isLessThan(4e-8);
+        assertThat(Maja.abs(Maja.erfcInv(Maja.erfc(-0.5)) + 0.5)).isLessThan(4e-8);
+        assertThat(Maja.abs(Maja.erfcInv(Maja.erfc(0.111)) - 0.111)).isLessThan(4e-8);
     }
 }
