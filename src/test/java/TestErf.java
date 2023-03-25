fb4d8acf15d11fc1be0afa75ed746663d6efd523
Kamila Szewczyk <kspalaiologos@gmail.com>
2023-03-25 18:42:38 +0100

    dawson +/- tests

--- src/test/java/TestErf.java
+++ src/test/java/TestErf.java
@@ -64,4 +64,37 @@ public class TestErf {
         assertThat(Maja.abs(Maja.erfcInv(Maja.erfc(-0.5)) + 0.5)).isLessThan(4e-8);
         assertThat(Maja.abs(Maja.erfcInv(Maja.erfc(0.111)) - 0.111)).isLessThan(4e-8);
     }
+
+    @Test
+    public void testDawson() {
+        assertThat(Maja.dawsonMinus(0.0)).isEqualTo(0.0);
+        assertThat(Maja.dawsonMinus(1.0)).isEqualTo(2.030078469278705);
+        assertThat(Maja.dawsonMinus(2.0)).isEqualTo(48.16001211429123);
+        assertThat(Maja.dawsonMinus(-1.0)).isEqualTo(-2.030078469278705);
+        assertThat(Maja.dawsonMinus(Double.NaN)).isNaN();
+        assertThat(Maja.dawsonMinus(Double.POSITIVE_INFINITY)).isEqualTo(Double.POSITIVE_INFINITY);
+        assertThat(Maja.dawsonMinus(Double.NEGATIVE_INFINITY)).isEqualTo(Double.NEGATIVE_INFINITY);
+        assertThat(Maja.dawsonMinus(0.5)).isEqualTo(0.5922965364693265);
+        assertThat(Maja.dawsonMinus(-0.5)).isEqualTo(-0.5922965364693265);
+        assertThat(Maja.dawsonMinus(1.5)).isEqualTo(8.123289304869214);
+        assertThat(Maja.dawsonMinus(12.34)).isEqualTo(1.2022582036795712E66);
+        assertThat(Maja.dawsonMinus(-6.77)).isEqualTo(-7.120664113754944E19);
+        assertThat(Maja.dawsonMinus(4.55)).isEqualTo(8.680134450911534E8);
+        assertThat(Maja.dawsonMinus(3.33)).isEqualTo(57995.18547618357);
+
+        assertThat(Maja.dawsonPlus(0.0)).isEqualTo(0.0);
+        assertThat(Maja.dawsonPlus(1.0)).isEqualTo(0.5380795069127684);
+        assertThat(Maja.dawsonPlus(2.0)).isEqualTo(0.301340388923792);
+        assertThat(Maja.dawsonPlus(-1.0)).isEqualTo(-0.5380795069127684);
+        assertThat(Maja.dawsonMinus(Double.NaN)).isNaN();
+        assertThat(Maja.dawsonPlus(Double.POSITIVE_INFINITY)).isEqualTo(0.0);
+        assertThat(Maja.dawsonPlus(Double.NEGATIVE_INFINITY)).isEqualTo(-0.0);
+        assertThat(Maja.dawsonPlus(0.5)).isEqualTo(0.4244363835020223);
+        assertThat(Maja.dawsonPlus(-0.5)).isEqualTo(-0.4244363835020223);
+        assertThat(Maja.dawsonPlus(1.5)).isEqualTo(0.42824907108539867);
+        assertThat(Maja.dawsonPlus(12.34)).isEqualTo(0.04065301492124149);
+        assertThat(Maja.dawsonPlus(-6.77)).isEqualTo(-0.07468887399692625);
+        assertThat(Maja.dawsonPlus(4.55)).isEqualTo(0.11276478411723788);
+        assertThat(Maja.dawsonPlus(3.33)).isEqualTo(0.1581685813429892);
+    }
 }
