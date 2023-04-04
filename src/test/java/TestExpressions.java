f8652b4b8f3809f974ccada652bde76cf78c18db
Kamila Szewczyk <kspalaiologos@gmail.com>
2023-04-04 19:28:14 +0200

    line gauss-legendre/tanh-sinh integrators for expression evaluator

--- src/test/java/TestExpressions.java
+++ src/test/java/TestExpressions.java
@@ -10,8 +10,9 @@ public class TestExpressions {
     public void testExpressions() {
         assertThat(Maja.eval("2 + 2")).isEqualTo(new Number(4));
         assertThat(Maja.eval("(sin(10) ** 2) + (cos(10) ** 2)")).isEqualTo(new Number(1.0));
-        assertThat(Maja.eval("simpson([exp(x)], -1, 1, dx, 10000)")).isEqualTo(
-                new Number(Maja.integrateSimpson((MonadicFunction) Maja::exp, -1, 1, 10000)));
+        assertThat(Maja.eval("simpson([exp(x)], -1, 1, dx, 10000)")).isEqualTo(new Number(Maja.integrateSimpson((MonadicFunction) Maja::exp, -1, 1, 10000)));
+        assertThat(Maja.eval("gausslegendre([exp(x)], -1, 1, dx, 5)")).isEqualTo(new Number(Maja.integrateGaussLegendre((MonadicFunction) Maja::exp, -1, 1, 5)));
+        assertThat(Maja.eval("tanhsinh([exp(x)], -1, 1, dx, 5, 1e-10)")).isEqualTo(new Number(Maja.integrateTanhSinh((MonadicFunction) Maja::exp, -1, 1, 5, 1e-10)[0]));
         assertThat(Maja.eval("pick(0, sin(1), sin(2), sin(3))")).isEqualTo(new Number(Maja.sin(1)));
         assertThat(Maja.eval("pick(1, sin(1), sin(2), sin(3))")).isEqualTo(new Number(Maja.sin(2)));
         assertThat(Maja.eval("pick(2, sin(1), sin(2), sin(3))")).isEqualTo(new Number(Maja.sin(3)));
@@ -20,5 +21,13 @@ public class TestExpressions {
         assertThat(Maja.eval("2*2").getLong()).isEqualTo(4);
         assertThat(Maja.eval("2 pi").getDouble()).isEqualTo(Maja.TWO_PI);
         assertThat(Maja.eval("2pi").getDouble()).isEqualTo(Maja.TWO_PI);
+        // test rem, mod, gcd, lcm:
+        assertThat(Maja.eval("10 rem 3").getLong()).isEqualTo(1);
+        assertThat(Maja.eval("10 mod 3").getLong()).isEqualTo(1);
+        assertThat(Maja.eval("10 mod -3").getLong()).isEqualTo(-2);
+        assertThat(Maja.eval("-10 mod 3").getLong()).isEqualTo(2);
+        // test factorial:
+        assertThat(Maja.eval("!0").getDouble()).isEqualTo(1);
+        assertThat(Maja.eval("!5").getDouble()).isEqualTo(120);
     }
 }
