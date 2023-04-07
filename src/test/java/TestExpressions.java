5551d23422f390b2f1dd7ba25cf4bae06d2dd80b
Kamila Szewczyk <kspalaiologos@gmail.com>
2023-04-07 16:15:46 +0200

    expression tests for line integrals

--- src/test/java/TestExpressions.java
+++ src/test/java/TestExpressions.java
@@ -1,9 +1,12 @@
 import org.junit.jupiter.api.Test;
+import rocks.palaiologos.maja.Complex;
 import rocks.palaiologos.maja.Maja;
 import rocks.palaiologos.maja.MonadicFunction;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import rocks.palaiologos.maja.Number;
+
+import java.util.function.Function;
 
 public class TestExpressions {
     @Test
@@ -13,6 +16,8 @@ public class TestExpressions {
         assertThat(Maja.eval("simpson([exp(x)], -1, 1, dx, 10000)")).isEqualTo(new Number(Maja.integrateSimpson((MonadicFunction) Maja::exp, -1, 1, 10000)));
         assertThat(Maja.eval("gausslegendre([exp(x)], -1, 1, dx, 5)")).isEqualTo(new Number(Maja.integrateGaussLegendre((MonadicFunction) Maja::exp, -1, 1, 5)));
         assertThat(Maja.eval("tanhsinh([exp(x)], -1, 1, dx, 5, 1e-10)")).isEqualTo(new Number(Maja.integrateTanhSinh((MonadicFunction) Maja::exp, -1, 1, 5, 1e-10)[0]));
+        assertThat(Maja.eval("linetanhsinh([exp(x)], -1+2i, 1-3i, dx, 5, 1e-10)")).isEqualTo(new Number(Maja.integrateTanhSinh(Maja::exp, new Complex(-1, 2), new Complex(1, -3), 5, 1e-10)[0]));
+        assertThat(Maja.eval("linegausslegendre([exp(x)], -1+2i, 1-3i, dx, 5)")).isEqualTo(new Number(Maja.integrateGaussLegendre(Maja::exp, new Complex(-1, 2), new Complex(1, -3), 5)));
         assertThat(Maja.eval("pick(0, sin(1), sin(2), sin(3))")).isEqualTo(new Number(Maja.sin(1)));
         assertThat(Maja.eval("pick(1, sin(1), sin(2), sin(3))")).isEqualTo(new Number(Maja.sin(2)));
         assertThat(Maja.eval("pick(2, sin(1), sin(2), sin(3))")).isEqualTo(new Number(Maja.sin(3)));
