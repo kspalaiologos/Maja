dfb8c1a85164b17a4aae87cad2c81df1b120b567
Kamila Szewczyk <kspalaiologos@gmail.com>
2023-05-13 16:41:37 +0200

    incomplete gamma tests

--- src/test/java/TestGamma.java
+++ src/test/java/TestGamma.java
@@ -29,4 +29,18 @@ public class TestGamma {
         assertThat(inRange(Maja.loggamma(new Complex(2, -3)), new Complex(-2.0928517530927333496, -2.3023965434668676262), 1e-10)).isTrue();
         assertThat(inRange(Maja.loggamma(new Complex(0.1, -3)), new Complex(-4.232218700260561, 0.3453402012115774), 1e-8)).isTrue();
     }
+
+    private void singlePQTest(double a, double b) {
+        assertThat(inRange(Maja.gammaP(a, b), Maja.liGamma(a, b) / Maja.gamma(a), 1e-10)).isTrue();
+        assertThat(inRange(Maja.gammaQ(a, b), 1 - Maja.gammaP(a, b), 1e-10)).isTrue();
+    }
+
+    @Test
+    public void testGammaP() {
+        singlePQTest(2, 3);
+        singlePQTest(2, 2.1);
+        singlePQTest(0.1, 1.1);
+        singlePQTest(0.1, 2.3);
+        singlePQTest(6, 4);
+    }
 }
